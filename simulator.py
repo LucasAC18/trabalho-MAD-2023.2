619851d025bae55795834894145be3751560fee2
Lucas Araujo Carvalho <lucasaraujo180701@hotmail.com>
2023-10-03 15:30:02 -0300

    refactoring code and adding references

--- simulator.py
+++ simulator.py
@@ -1,65 +1,6 @@
-from numpy.random import poisson, exponential
-import math
+# The main simulation function is defined here, along with a main to test it
 
-# main class for implementing the event queue
-# events are represented by a tuple => (type, event_time)
-class Queue():
-
-  def __init__(self):
-    self.queue = []
-
-  def __str__(self):
-    return str(self.queue)
-
-  def push(self, new_element):
-    self.queue.append(new_element)
-    self.queue.sort(key=lambda e: e[1])
-
-  def pop(self):
-    served = self.queue.pop(0)
-    return served
-
-  def is_empty(self):
-    return len(self.queue) == 0
-
-# class to store the metrics obtained from the simulation
-class Statistics():
-
-  def __init__(self, mcpr, mwtpr, mestpr):
-    self.mean_customers_per_round = mcpr
-    self.mean_customers_in_system = sum(mcpr)/len(mcpr)
-    self.mean_customers_interval = self.get_confidence_interval(self.mean_customers_per_round, self.mean_customers_in_system, len(mcpr))
-
-    self.mean_wait_time_per_round = mwtpr
-    self.mean_wait_time_in_system = sum(mwtpr)/len(mwtpr)
-    self.mean_wait_time_interval = self.get_confidence_interval(self.mean_wait_time_per_round, self.mean_wait_time_in_system, len(mwtpr))
-
-    self.mean_empty_state_time_per_round = mestpr
-    self.mean_empty_state_time_in_system = sum(mestpr)/len(mestpr)
-    self.mean_empty_state_interval = self.get_confidence_interval(self.mean_empty_state_time_per_round, self.mean_empty_state_time_in_system, len(mestpr))
-
-  def get_confidence_interval(self, metric_per_round, metric_mean, N):
-    sd = [(val-metric_mean)**2 for val in metric_per_round]
-    sd = sum(sd)/max((N-1),1)
-    sd = math.sqrt(sd)
-
-    return [metric_mean-1.96*sd/math.sqrt(N),metric_mean+1.96*sd/math.sqrt(N)]
-
-# event types
-TYPES = ["Arrival", "Departure"]
-
-# distribution functions for arrival and departure
-def arrival_dist(_lambda):
-  return exponential(1/_lambda)
-
-def departure_dist(_mu):
-  return exponential(1/_mu)
-
-# Little Law formula to calculate mean value of number of clients on the system
-def littles_law(_lambda, _mu):
-  rho = _lambda/_mu
-
-  return rho/(1-rho)
+from helpers import *
 
 # function to properly simulate the queue
 def simulate(_lambda, _mu, total_time = 10, total_rounds = 1):
