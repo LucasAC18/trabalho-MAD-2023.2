139e1d151f75e853ada2dc0d340ea945926b41a0
Lucas Araujo Carvalho <lucasaraujo180701@hotmail.com>
2023-10-05 20:38:56 -0300

    add fraction of times it hit state 0

--- helpers.py
+++ helpers.py
@@ -27,7 +27,7 @@ class Queue():
 # class to store the metrics obtained from the simulation
 class Statistics():
 
-  def __init__(self, mcpr, mwtpr, mestpr):
+  def __init__(self, mcpr, mwtpr, mestpr, fthepr):
     self.mean_customers_per_round = mcpr
     self.mean_customers_in_system = sum(mcpr)/len(mcpr)
     self.mean_customers_interval = self.get_confidence_interval(self.mean_customers_per_round, self.mean_customers_in_system, len(mcpr))
@@ -40,12 +40,22 @@ class Statistics():
     self.mean_empty_state_time_in_system = sum(mestpr)/len(mestpr)
     self.mean_empty_state_interval = self.get_confidence_interval(self.mean_empty_state_time_per_round, self.mean_empty_state_time_in_system, len(mestpr))
 
+    self.fraction_times_hit_empty_per_round = fthepr
+    self.fraction_times_hit_empty_in_system = sum(fthepr)/len(fthepr)
+    self.fraction_times_hit_empty_interval = self.get_confidence_interval(self.fraction_times_hit_empty_per_round, self.fraction_times_hit_empty_in_system, len(fthepr))
+
   def get_confidence_interval(self, metric_per_round, metric_mean, N):
     sd = [(val-metric_mean)**2 for val in metric_per_round]
     sd = sum(sd)/max((N-1),1)
     sd = math.sqrt(sd)
 
     return [metric_mean-1.96*sd/math.sqrt(N),metric_mean+1.96*sd/math.sqrt(N)]
+  
+  def __str__(self):
+    return f'Mean customers in system    => {self.mean_customers_in_system}\nConfidence Interval         => {self.mean_customers_interval}\n\n' + \
+           f'Mean wait time in system    => {self.mean_wait_time_in_system}\nConfidence Interval         => {self.mean_wait_time_interval}\n\n' + \
+           f'Mean 0 state time in system => {self.mean_empty_state_time_in_system}\nConfidence Interval         => {self.mean_empty_state_interval}\n\n' + \
+           f'Fraction of times 0 state   => {self.fraction_times_hit_empty_in_system}\nConfidence Interval         => {self.fraction_times_hit_empty_interval}\n'
 
 # event types constants
 TYPES = ["Arrival", "Departure"]
@@ -57,7 +67,7 @@ def arrival_dist(_lambda):
 def departure_dist(_mu):
   return exponential(1/_mu)
 
-# Little Law formula to calculate mean value of number of clients on the system analytically
+# Little's Law formula to calculate mean value of number of clients on the system analytically
 def littles_law(_lambda, _mu):
   rho = _lambda/_mu
 
